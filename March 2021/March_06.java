78c082d81d47d4c58c68c7d3164d97f0e35e0d2c
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2021-03-17 01:01:13 +0530

    Create March_06.java

---
+++ March 2021/March_06.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet(Arrays.asList(words));
+        for(String word: words) for(int k=1; k<word.length(); k++) set.remove(word.substring(k));
+        int ans = 0;
+        for(String word: set) ans += word.length()+1;
+        return ans;
+    }
+}
