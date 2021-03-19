5c00e16b47f60773db885474ada24787ed5813e3
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2021-03-19 15:41:50 +0530

    Create March_14.java

---
+++ March 2021/March_14.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode firstN = head;
+        while(--k > 0) firstN = firstN.next;
+        ListNode f = firstN;
+        ListNode secondN = head;
+        while(f.next!=null){
+            secondN = secondN.next;
+            f = f.next;
+        }
+        int temp = firstN.val;
+        firstN.val = secondN.val;
+        secondN.val = temp;
+        return head;
+    }
+}
