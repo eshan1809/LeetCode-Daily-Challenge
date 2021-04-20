d8c20d388ada82e96a85a641da11ff9e1064861a
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2021-04-20 14:30:20 +0530

    Create April_20.java

---
+++ April 2021/April_20.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+  
+    List<Integer> list;
+  
+    public List<Integer> preorder(Node root) {
+        list = new ArrayList<>();
+        getInOrder(root);
+        return list;
+    }
+  
+    public void getInOrder(Node root){
+        if(root == null) return;
+        list.add(root.val);
+        for(Node child: root.children)
+            getInOrder(child);
+    }
+}
