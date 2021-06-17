8726f2fdd8bdb594ef93225d7180ae2e4564d5e2
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2021-06-17 13:13:32 +0530

    Update June_17.java

--- June 2021/June_17.java
+++ June 2021/June_17.java
@@ -2,10 +2,9 @@ class Solution {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
         int start = -1, end = -1, ans = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] > right){
-                start = i;
-                end = i;
-            } else if(nums[i] >= left && nums[i] <= right)
+            if(nums[i] > right)
+                start = end = i;
+            else if(nums[i] >= left && nums[i] <= right)
                 end = i;
             ans += end - start;
         }
