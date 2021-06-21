cc65f3d70b129f1bb37a07b45e114fe41a2eeccf
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2021-06-21 12:58:03 +0530

    Create June_21.java

---
+++ June 2021/June_21.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        for(int i = 0; i < numRows; i++){
+            List<Integer> temp = new ArrayList<Integer>();
+            for(int j = 0; j <= i; j++)
+                if(j == 0 || j == i) temp.add(1);
+                else temp.add(list.get(i - 1).get(j - 1) + list.get(i - 1).get(j));
+            list.add(temp);
+        }
+        return list;
+    }
+}
