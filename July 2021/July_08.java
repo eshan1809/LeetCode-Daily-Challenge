f875bf27dc06139805476e5c4f2915a4e13335d4
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2021-07-08 12:59:52 +0530

    Create July_08.java

---
+++ July 2021/July_08.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findLength(int[] arr1, int[] arr2) {
+        int n = arr1.length, m = arr2.length, max = 0;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++)
+                if (arr1[i - 1] == arr2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+        return max;
+    }
+}
