8cbf01f76eae8901b40d33ed0964ad4a3751f9da
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-02-03 09:03:29 +0530

    Create February_03.java

---
+++ February 2022/February_03.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums1)
+            for(int j : nums2)
+                map.put(i + j, map.getOrDefault(i + j, 0) + 1);
+        int count = 0;
+        for(int i : nums3)
+            for(int j : nums4)
+                count += map.getOrDefault(-i - j, 0);
+        return count;
+    }
+}
