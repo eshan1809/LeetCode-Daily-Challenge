efc2cc052f06833c5bd79081202903177117789a
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-02-08 08:55:05 +0530

    Create February_08.java

---
+++ February 2022/February_08.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num >= 10){
+            int x = 0;
+            while(num > 0){
+                x += num % 10;
+                num /= 10;
+            }
+            num = x;
+        }
+        return num;
+    }
+}
