1c628b5190adb2c0838302108f0102b44ea0fc94
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-03-03 08:18:22 +0530

    Create March_03.java

---
+++ March 2022/March_03.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length, ans = 0;
+        int[] dp = new int[n];
+        for(int i = 2; i < n; i++){
+            if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2])
+                dp[i] = dp[i - 1] + 1;
+            ans += dp[i];
+        }
+        return ans;
+    }
+}
