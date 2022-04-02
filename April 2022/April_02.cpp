873c2d047276ac30fd1434878e64c5cdcbd30d22
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-04-02 09:38:25 +0530

    Create April_02.cpp

---
+++ April 2022/April_02.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while(i < j){
+            if(s[i] != s[j]){
+                if(isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j))
+                    return true;
+                return false;
+            }
+            i++, j--;
+        }
+        return true;
+    }
+    
+    bool isPalindrome(string s, int i, int j){
+        while(i < j)
+            if(s[i++] != s[j--])
+                return false;
+        return true;
+    }
+};
