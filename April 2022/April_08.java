922cdf95767ba18b4bbca27f747fd8e4b475382e
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-04-08 11:00:06 +0530

    Create April_08.java

---
+++ April 2022/April_08.java
@@ -0,0 +1,24 @@
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>();
+        this.k = k;
+        for(int i : nums)
+            pq.add(i);
+    }
+    
+    public int add(int val) {
+        pq.add(val);
+        while(pq.size() > k)
+            pq.remove();
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
