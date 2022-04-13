62cbb2fad7899f4158f66c188e860536ca7dcbf5
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-04-13 09:59:28 +0530

    Create April_13.cpp

---
+++ April 2022/April_13.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int> > mat(n, vector<int>(n));
+        int rb = 0, cb = 0, re = n - 1, ce = n - 1, p = 1;
+        
+        while(true){
+            
+            for(int i = cb; i <= ce; i++)
+                mat[rb][i] = p++;
+            
+            rb++;
+            if(rb > re)
+                break;
+            
+            for(int i = rb; i <= re; i++)
+                mat[i][ce] = p++;
+            
+            ce--;
+            if(cb > ce)
+                break;
+            
+            for(int i = ce; i >= cb; i--)
+                mat[re][i] = p++;
+            
+            re--;
+            if(rb > re)
+                break;
+            
+            for(int i = re; i >= rb; i--)
+                mat[i][cb] = p++;
+            
+            cb++;
+            if(cb > ce)
+                break;
+            
+        }
+        
+        return mat;
+    }
+};
