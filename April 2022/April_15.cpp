2aa685a7e0f2f67c4f0382da679c7ba0d30f4662
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-04-15 09:05:36 +0530

    Create April_15.cpp

---
+++ April 2022/April_15.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int lo, int hi) {
+        if(root == nullptr)
+            return root;
+        if(root->val < lo)
+            return trimBST(root->right, lo, hi);
+        if(root->val > hi)
+            return trimBST(root->left, lo, hi);
+        root->left = trimBST(root->left, lo, hi);
+        root->right = trimBST(root->right, lo, hi);
+        return root;
+    }
+};
