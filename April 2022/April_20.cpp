5f6d68b2f5f3f25d2d0a419163e6a1beabc0a4e2
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-04-20 12:56:23 +0530

    Create April_20.cpp

---
+++ April 2022/April_20.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+    
+    BSTIterator(TreeNode* root) {
+        while(root != nullptr){
+            st.push(root);
+            root = root->left;
+        }
+    }
+    
+    int next() {
+        TreeNode *rem = st.top(), *add = rem->right; st.pop();
+        while(add != nullptr){
+            st.push(add);
+            add = add->left;
+        }
+        return rem->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
