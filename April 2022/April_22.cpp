60d97bf970d14b42435663e54736273d3f3080d6
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-04-22 07:28:17 +0530

    Create April_22.cpp

---
+++ April 2022/April_22.cpp
@@ -0,0 +1,29 @@
+class MyHashMap {
+public:
+    
+    int map[(int)1e6 + 1];
+    
+    MyHashMap() {
+        memset(map, -1, sizeof(map));
+    }
+    
+    void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    int get(int key) {
+        return map[key];
+    }
+    
+    void remove(int key) {
+        map[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
