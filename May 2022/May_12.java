a34de088eca1b17ce5c17ddde9948c853b52fe7a
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-05-12 12:08:59 +0530

    Update May_12.java

--- May 2022/May_12.java
+++ May 2022/May_12.java
@@ -4,13 +4,13 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i : nums)
             map.put(i, map.getOrDefault(i, 0) + 1);
-        helper(nums.length, nums, new ArrayList<>(), map);
+        helper(nums, new ArrayList<>(), map);
         return list;
     }
     
     List<List<Integer>> list;
     
-    private void helper(int n, int[] nums, List<Integer> al, Map<Integer, Integer> map){
+    private void helper(int[] nums, List<Integer> al, Map<Integer, Integer> map){
         if(map.isEmpty()){
             list.add(new ArrayList<>(al));
             return;
@@ -22,13 +22,13 @@ class Solution {
             if(f == 1){
                 nmap.remove(i);
                 al.add(i);
-                helper(n, nums, al, nmap);
+                helper(nums, al, nmap);
                 al.remove(al.size() - 1);
                 nmap.put(i, 1);
             } else{
                 nmap.put(i, f - 1);
                 al.add(i);
-                helper(n, nums, al, nmap);
+                helper(nums, al, nmap);
                 al.remove(al.size() - 1);
                 nmap.put(i, f);
             }
