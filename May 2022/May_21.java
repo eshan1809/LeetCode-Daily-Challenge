bc1876d800a7267bf610452ba923bf73f3b1d0da
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-05-21 10:25:32 +0530

    Update May_21.java

--- May 2022/May_21.java
+++ May 2022/May_21.java
@@ -1,7 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
-        dp = new Integer[amount + 1][coins.length];
+        dp = new Integer[coins.length][amount + 1];
         int ans = helper(coins, amount, coins.length - 1);
         return ans >= 100 ? -1 : ans;
     }
@@ -13,8 +13,8 @@ class Solution {
             return 0;
         if(amount < 0 || i < 0)
             return 100;
-        if(dp[amount][i] != null)
-            return dp[amount][i];
-        return dp[amount][i] = Math.min(1 + helper(coins, amount - coins[i], i), helper(coins, amount, i - 1));
+        if(dp[i][amount] != null)
+            return dp[i][amount];
+        return dp[i][amount] = Math.min(1 + helper(coins, amount - coins[i], i), helper(coins, amount, i - 1));
     }
 }
