9db572a4416e8423b8827e21dff313b59ab086a6
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-05-27 12:50:45 +0530

    Create May_27.cpp

---
+++ May 2022/May_27.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int ans = 0;
+        while(num){
+            if(num & 1)
+                num--;
+            else
+                num >>= 1;
+            ans++;
+        }
+        return ans;
+    }
+};
