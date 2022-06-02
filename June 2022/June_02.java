8c7f88f32a866cbe44aa235f7f2916864e806c66
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-06-02 07:16:39 +0530

    Create June_02.java

---
+++ June 2022/June_02.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        int[][] trans = new int[m][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                trans[j][i] = matrix[i][j];
+        return trans;
+    }
+}
