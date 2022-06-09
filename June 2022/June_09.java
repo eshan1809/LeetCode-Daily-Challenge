a48e1a921f598f8489c6775804a72edfa5ca298b
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-06-09 10:10:49 +0530

    Create June_09.java

---
+++ June 2022/June_09.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while(i < j){
+            if(nums[i] + nums[j] > target)
+                j--;
+            else if(nums[i] + nums[j] < target)
+                i++;
+            else
+                return new int[]{i + 1, j + 1};
+        }
+        return null;
+    }
+}
