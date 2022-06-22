b8fa576076101e9852b6860e58339629880bd765
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-06-22 10:25:51 +0530

    Create June_22.cpp

---
+++ June 2022/June_22.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int i = 0; i <= nums.size() - k; i++)
+            pq.push(nums[i]);
+        for(int i = nums.size() - k + 1; i < nums.size(); i++)
+            if(nums[i] < pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        return pq.top();
+    }
+};
