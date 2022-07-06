163c367866250c17945155b9985a97fa0af286c5
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-07-06 10:50:54 +0530

    Create July_06.cpp

---
+++ July 2022/July_06.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1)
+            return n;
+        int f = 0, s = 1;
+        for(int i = 2; i <= n; i++){
+            int c = f + s;
+            f = s;
+            s = c;
+        }
+        return s;
+    }
+};
