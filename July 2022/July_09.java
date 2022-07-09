0da77b88dea727f1ba4c37c848d3ceb5def568c5
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-07-09 10:16:15 +0530

    Update July_09.java

--- July 2022/July_09.java
+++ July 2022/July_09.java
@@ -1,25 +1,15 @@
 class Solution {
     public int maxResult(int[] nums, int k) {
-        int n = nums.length, i = n - 1, max = (int) 1e9, j;
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         int[] dp = new int[n];
-        dp[i] = nums[i];
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        map.put(dp[i], 1);
-        for(i = n - 2, j = 1; i >= 0 && j <= k; i--, j++){
-            Integer key = map.floorKey(max);
-            dp[i] = nums[i] + key;
-            map.put(dp[i], map.getOrDefault(dp[i], 0) + 1);
-        }
-        while(i >= 0){
-            int f = map.get(dp[i + k + 1]);
-            if(f == 1)
-                map.remove(dp[i + k + 1]);
-            else
-                map.put(dp[i + k + 1], map.getOrDefault(dp[i + k + 1], 0) - 1);
-            Integer key = map.floorKey(max);
-            dp[i] = nums[i] + key;
-            map.put(dp[i], map.getOrDefault(dp[i], 0) + 1);
-            i--;
+        dp[n - 1] = nums[n - 1];
+        pq.add(new int[]{dp[n - 1], n - 1});
+        for(int i = n - 2; i >= 0; i--){
+            while(pq.peek()[1] > i + k)
+                pq.remove();
+            dp[i] = nums[i] + pq.peek()[0];
+            pq.add(new int[]{dp[i], i});
         }
         return dp[0];
     }
