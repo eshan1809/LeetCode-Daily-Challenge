039b2167ebe0a14abf9fbc08f42270695b99b3df
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-07-12 10:58:59 +0530

    Create July_12.java

---
+++ July 2022/July_12.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        long sum = 0L;
+        for(int i : matchsticks)
+            sum += i;
+        if(sum % 4 != 0)
+            return false;
+        side = (int)(sum / 4);
+        n = matchsticks.length;
+        used = new boolean[n];
+        return helper(0, matchsticks, 0, 0);
+    }
+    
+    int n, side;
+    boolean[] used;
+    
+    private boolean helper(int i, int[] arr, int sum, int f){
+        if(i == n){
+            if(sum != side)
+                return false;
+            if(f == 3)
+                return true;
+            return helper(0, arr, 0, f + 1);
+        }
+        if(helper(i + 1, arr, sum, f))
+            return true;
+        if(!used[i] && sum + arr[i] <= side){
+            used[i] = true;
+            if(helper(i + 1, arr, sum + arr[i], f))
+                return true;
+            used[i] = false;
+        }
+        return false;
+    }
+}
