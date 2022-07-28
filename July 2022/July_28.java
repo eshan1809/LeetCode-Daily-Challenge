0aa80cbd67c911702b29c1c8f0743a7df6f083c0
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-07-28 11:18:18 +0530

    Create July_28.java

---
+++ July 2022/July_28.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        for(char ch : s.toCharArray())
+            arr[ch - 'a']++;
+        for(char ch : t.toCharArray())
+            arr[ch - 'a']--;
+        for(int i : arr)
+            if(i != 0)
+                return false;
+        return true;
+    }
+}
