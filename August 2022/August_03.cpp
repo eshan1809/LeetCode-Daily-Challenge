7f574cfa83e34593cbbd1cf82498aff2b293f049
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-08-03 10:37:16 +0530

    Create August_03.cpp

---
+++ August 2022/August_03.cpp
@@ -0,0 +1,23 @@
+class MyCalendar {
+public:
+    MyCalendar() {
+        
+    }
+    
+    map<int, int> mp;
+    
+    bool book(int start, int end) {
+        auto it = mp.upper_bound(start);
+        if (it == mp.end() || it->second >= end){
+            mp[end] = start;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
