93679b3b1c1f1a985b147c2b71d56fdefeec81ff
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-08-16 13:13:17 +0530

    Create August_16.cpp

---
+++ August 2022/August_16.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> arr(26, 0);
+        for(char &ch : s)
+            arr[ch - 'a']++;
+        for(int i = 0; i < s.length(); i++)
+            if(arr[s[i] - 'a'] == 1)
+                return i;
+        return -1;
+    }
+};
