9d3508cb4e8dfe851f8713c47d020843722cb335
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-08-17 12:36:41 +0530

    Create August_17.java

---
+++ August 2022/August_17.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] arr = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+        Set<String> set = new HashSet<>();
+        for(String word : words){
+            StringBuilder sb = new StringBuilder();
+            for(char ch : word.toCharArray())
+                sb.append(arr[ch - 'a']);
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
