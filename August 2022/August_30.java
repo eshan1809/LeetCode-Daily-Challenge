34a8067b19274b1df4301ade27c8264982e9d1a7
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-08-30 10:51:28 +0530

    Create August_30.java

---
+++ August 2022/August_30.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < n / 2; i++){
+            int e = n - i - 1;
+            for(int j = i, k = 0; j < e; j++, k++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[e - k][i];
+                matrix[e - k][i] = matrix[e][e - k];
+                matrix[e][e - k] = matrix[j][e];
+                matrix[j][e] = tmp;
+            }
+        }
+    }
+}
