ea8ac139d634ef5e85ae3e81e40f07b751a6b0e1
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-09-02 14:18:05 +0530

    Create September_02.java

---
+++ September 2022/September_02.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> list = new ArrayList<>();
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            long sum = 0L;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.remove();
+                sum += node.val;
+                if(node.left != null)
+                    q.add(node.left);
+                if(node.right != null)
+                    q.add(node.right);
+            }
+            list.add(1.0 * sum / size);
+        }
+        return list;
+    }
+}
