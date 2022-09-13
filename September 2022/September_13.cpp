13b4aa54442bb1ed0d90fccc0dd955669ab4acdd
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2022-09-13 11:24:18 +0530

    Create September_13.cpp

---
+++ September 2022/September_13.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int rem = 0;
+        for (auto &val : data) {
+            if ((val >> 7) == 0b0) {
+                if (rem > 0)
+                    return false;
+            } else if ((val >> 5) == 0b110) {
+                if (rem > 0)
+                    return false;
+                rem = 1;
+            } else if ((val >> 4) == 0b1110) {
+                if (rem > 0)
+                    return false;
+                rem = 2;
+            } else if ((val >> 3) == 0b11110) {
+                if (rem > 0)
+                    return false;
+                rem = 3;
+            } else {
+                if ((val >> 6) == 0b10 && rem > 0)
+                    rem--;
+                else
+                    return false;
+            }
+        }
+        return rem == 0;
+    }
+};
